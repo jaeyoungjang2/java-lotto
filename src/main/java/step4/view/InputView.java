a28b3b8f5adf4dd6d35fa72f1b548c28aa1815ef
jaeyoungjang2 <lion85488@gmail.com>
2021-09-09 07:33:25 +0900

    feat: InputView 생성 수동으로 구매할 로또 수 입력 받기

--- src/main/java/step4/view/InputView.java
+++ src/main/java/step4/view/InputView.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_MESSAGE = "구입금액을 입력해 주세요.";
+    private static final String NUMBER_OF_MANUAL_LOTTO = "구입금액을 입력해 주세요.";
     private static final Scanner SC = new Scanner(System.in);
 
     public static Integer requirePurchaseAmount() {
         System.out.println(PURCHASE_AMOUNT_MESSAGE);
+        return SC.nextInt();
+    }
+
+    public static Integer requireNumberOfManualLotto() {
+        System.out.println(NUMBER_OF_MANUAL_LOTTO);
         return SC.nextInt();
     }
 }
